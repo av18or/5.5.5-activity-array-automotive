3d567a80aa3d03ca51ebb97ae3a1e2b404e4c3ee
Clayton <claytongorham@gmail.com>
2022-01-23 19:13:13 -0800

    updates

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,28 @@
+
+
 //this includes the vehicle class as a module
 const VehicleModule = require("./vehicle")
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 6;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 200;
+        this.fuel = 100;
+        scheduleService = false;
+    }
+
+
+
+    loadPassenger(num) {
+        if (this.passenger < this.maxPassenger) {
+            if ((num + this.passenger) <= this.maxPassengers)
+        }
+    }
+}
